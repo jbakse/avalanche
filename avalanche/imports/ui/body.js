c30f77254501ca7fa534597aac41455728c548b8
Justin Bakse <justin@tsfim.com>
2016-10-23 13:41:07 -0400

    Posts: Create, Date Order, Remove

--- avalanche/imports/ui/body.js
+++ avalanche/imports/ui/body.js
@@ -1,11 +1,36 @@
-import { Template } from 'meteor/templating';
+import {
+    Template
+} from 'meteor/templating';
 
-import { Posts } from '../api/posts.js';
+import {
+    Posts
+} from '../api/posts.js';
 
 import './body.html';
 
 Template.body.helpers({
-  posts() {
-    return Posts.find({});
-  }
+    posts() {
+        return Posts.find({}, {sort: {createdAt: -1}});
+    }
+});
+
+Template.body.events({
+    'click #create-post': function(event) {
+        event.preventDefault();
+
+        Posts.insert({
+            title: 'untitled' + Math.floor(Math.random()*100),
+            author: "unauthored",
+            poster: "",
+            description: "",
+            time: "",
+            lesson: "",
+            createdAt: Date.now()
+        });
+
+    },
+    'click .kill-post': function(event) {
+      console.log("kill kill kill", this);
+      Posts.remove(this._id);
+    }
 });
