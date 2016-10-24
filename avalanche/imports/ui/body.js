c994b79aa7f6b77bea052a077a2888a4300ca6d2
Justin Bakse <justin@tsfim.com>
2016-10-23 22:02:43 -0400

    working on making titles editable with content editable

--- avalanche/imports/ui/body.js
+++ avalanche/imports/ui/body.js
@@ -1,36 +1,59 @@
 import {
-    Template
+	Template
 } from 'meteor/templating';
 
 import {
-    Posts
+	Posts
 } from '../api/posts.js';
 
 import './body.html';
 
 Template.body.helpers({
-    posts() {
-        return Posts.find({}, {sort: {createdAt: -1}});
-    }
+	posts() {
+		return Posts.find({}, {
+			sort: {
+				createdAt: -1
+			}
+		});
+	}
 });
 
+Template.post.editableTitle = function () {
+	console.log("editableTitle");
+  return '<div class="title" contenteditable="true">' + this.title + '</div>';
+};
+
+
 Template.body.events({
-    'click #create-post': function(event) {
-        event.preventDefault();
+	'click #create-post': function(event) {
+		event.preventDefault();
 
-        Posts.insert({
-            title: 'untitled' + Math.floor(Math.random()*100),
-            author: "unauthored",
-            poster: "",
-            description: "",
-            time: "",
-            lesson: "",
-            createdAt: Date.now()
-        });
+		Posts.insert({
+			title: 'untitled' + Math.floor(Math.random() * 100),
+			author: "unauthored",
+			poster: "",
+			description: "",
+			time: "",
+			lesson: "",
+			createdAt: Date.now()
+		});
 
-    },
-    'click .kill-post': function(event) {
-      console.log("kill kill kill", this);
-      Posts.remove(this._id);
-    }
+	},
+
+
+	'blur .post .title': function(event) {
+		let t = event.target.innerText;
+		t = t.trim();
+		console.log("edit", this);
+		Posts.update(this._id, {
+			$set: {title: t},
+		});
+		// event.target.innerText = t;
+
+	},
+
+	'click .kill-post': function(event) {
+		console.log("kill kill kill", this);
+		Posts.remove(this._id);
+	}
 });
