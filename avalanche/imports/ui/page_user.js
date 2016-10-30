31896b6ca590a83339d1ddebaed2d61aca023bec
Justin Bakse <justin@tsfim.com>
2016-10-30 17:01:23 -0400

    experimented with auto form

--- avalanche/imports/ui/page_user.js
+++ avalanche/imports/ui/page_user.js
@@ -1,11 +1,15 @@
 import { Template } from 'meteor/templating';
 import {Posts} from '../api/posts.js';
 
+import '../api/users.js';
+
 import './page_user.html';
 
 Template.page_user.helpers({
 	user() {
 		let id = FlowRouter.getParam("userId");
+
+		console.log(Meteor.users.findOne(id));
 		return Meteor.users.findOne(id);
 	},
 
