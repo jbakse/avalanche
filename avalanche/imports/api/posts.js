9ae77e256958cc2818ad7ed816f3f077f9c96ab6
Justin Bakse <justin@tsfim.com>
2016-11-02 19:50:52 -0400

    scheming

--- avalanche/imports/api/posts.js
+++ avalanche/imports/api/posts.js
@@ -13,4 +13,31 @@ import {
 	Mongo
 } from 'meteor/mongo';
 
+
+PostSchema = new SimpleSchema({
+	title: {
+		type: String,
+		label: "Title",
+        defaultValue: "",
+		max: 200
+	},
+    last_name: {
+        type: String,
+        label: "Last Name",
+        defaultValue: "",
+        max: 200
+    },
+    email: {
+        type: String,
+        label: "Email",
+        defaultValue: "",
+        regEx: SimpleSchema.RegEx.Email,
+        max: 200
+    },
+});
+
+
+
+
+
 export const Posts = new Mongo.Collection('posts');
