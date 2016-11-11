d1f5c0c4834e0da3b64b8d32c049fc1593ebf03f
Justin Bakse <justin@tsfim.com>
2016-11-11 11:00:14 -0500

    moved api keys to settings.json

--- avalanche/server/main.js
+++ avalanche/server/main.js
@@ -1,6 +1,12 @@
-import '../imports/api/posts.js';
-import '../imports/api/users.js';
-import './secrets.js';
+import "../imports/api/posts.js";
+import "../imports/api/users.js";
+// import "./secrets.js";
+
+Cloudinary.config ({
+	cloud_name: Meteor.settings.cloud_name,
+	api_key: Meteor.settings.api_key,
+	api_secret: Meteor.settings.api_secret,
+});
 
 
 // import { Meteor } from 'meteor/meteor';
