4476f6bb7d547c5d4f90cc5af7b08d3da492698c
Justin Bakse <justin@tsfim.com>
2016-12-22 01:33:09 -0500

    better counting

--- avalanche/imports/ui/page_avalanche.js
+++ avalanche/imports/ui/page_avalanche.js
@@ -1,8 +1,8 @@
 import {Template} from "meteor/templating";
 
+import {getPrefs} from "../api/prefs.js";
 import {Posts} from "../api/posts.js";
 import "./page_avalanche.html";
-
 
 function posts() {
 	let posts = Posts.find({
@@ -21,5 +21,33 @@ Template.page_avalanche.helpers({
 	},
 	postsCount() {
 		return posts().count();
+	},
+	posts_this_week() {
+		let prefs = getPrefs();
+		if (!prefs) {
+			return;
+		}
+		let weeks = prefs.weeks;
+		let week = _.find(weeks, function(week) {
+			return week.start < new Date() && week.end > new Date();
+		});
+		if (!week) {
+			return;
+		}
+
+
+
+		let posts = Posts.find({
+			
+			"created_at":
+			{
+				$gte: week.start,
+				$lt: week.end,
+			}
+		});
+
+
+
+		return posts.count();
 	}
 });
