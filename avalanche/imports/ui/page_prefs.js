79958ac3f36561c1895c776a5cf5f4dc4cdbe863
Justin Bakse <justin@tsfim.com>
2017-01-13 17:53:19 -0500

    hide prefs from non admins

--- avalanche/imports/ui/page_prefs.js
+++ avalanche/imports/ui/page_prefs.js
@@ -3,3 +3,14 @@ import {Prefs} from "../api/prefs.js";
 import "./page_prefs.html";
 
 window.Prefs = Prefs;
+
+
+Template.page_prefs.helpers({
+	userIsAdmin() {
+		return Roles.userIsInRole(Meteor.userId(), ["admin"], Roles.GLOBAL_GROUP);
+	},
+	userMayAdmin() {
+		return Roles.userIsInRole(Meteor.userId(), ["may_admin"], Roles.GLOBAL_GROUP);
+	}
+
+});
