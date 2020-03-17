7e1a2c34d1cb4cae6fd444534cf95482fef7105e
Justin Bakse <justin@tsfim.com>
2020-03-17 14:02:10 -0400

    add comment button

--- avalanche/imports/ui/posts.js
+++ avalanche/imports/ui/posts.js
@@ -3,53 +3,52 @@ import { Posts, postEditableBy } from "../api/posts.js";
 import "./posts.html";
 
 function updateMarker() {
-	if ($(window).scrollTop() < 500) {
-		$(".posts-marker").addClass("hidden");
-		return;
-	} else {
-		$(".posts-marker").removeClass("hidden");
-	}
-	let last_visible = false;
+  if ($(window).scrollTop() < 500) {
+    $(".posts-marker").addClass("hidden");
+    return;
+  } else {
+    $(".posts-marker").removeClass("hidden");
+  }
+  let last_visible = false;
 
-	$(".post").each(function(i, post) {
-		let y = $(post).offset().top;
-		y = y - $(window).scrollTop();
-		if (y > $(window).height()) {
-			return false;
-		}
-		last_visible = $(post);
-	});
+  $(".post").each(function(i, post) {
+    let y = $(post).offset().top;
+    y = y - $(window).scrollTop();
+    if (y > $(window).height()) {
+      return false;
+    }
+    last_visible = $(post);
+  });
 
-	// console.log(last_visible);
-	if (!last_visible) return;
-	// $(".post").removeClass("debug-highlight");
-	// last_visible.addClass("debug-highlight");
-	let post = Posts.findOne(last_visible.attr("id"));
-	if (!post) return;
+  // console.log(last_visible);
+  if (!last_visible) return;
+  // $(".post").removeClass("debug-highlight");
+  // last_visible.addClass("debug-highlight");
+  let post = Posts.findOne(last_visible.attr("id"));
+  if (!post) return;
 
-	let week = weekForDate(post.created_at);
-	if (!week) return;
-	$(".posts-marker").html(`Week ${week.num}: ${week.topic}`);
+  let week = weekForDate(post.created_at);
+  if (!week) return;
+  $(".posts-marker").html(`Week ${week.num}: ${week.topic}`);
 }
 
 $(window).on("scroll", updateMarker);
 
 //https://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling/488073#488073
 function isScrolledIntoView(elem) {
-	let docViewTop = $(window).scrollTop();
-	let docViewBottom = docViewTop + $(window).height();
+  let docViewTop = $(window).scrollTop();
+  let docViewBottom = docViewTop + $(window).height();
 
-	let elemTop = $(elem).offset().top;
-	let elemBottom = elemTop + $(elem).height();
+  let elemTop = $(elem).offset().top;
+  let elemBottom = elemTop + $(elem).height();
 
-	// return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-	return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+  // return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+  return elemTop <= docViewBottom && elemBottom >= docViewTop;
 }
 
 // function autoplayVideoPosters() {
 // 	console.log("autoplay");
 // 	let videos = $("video");
-
 
 // 	// let playCount = 0;
 // 	// let pauseCount = 0;
@@ -80,332 +79,353 @@ function isScrolledIntoView(elem) {
 // $(window).on("scroll", autoplayVideoPosters);
 // setInterval(autoplayVideoPosters, 1000);
 
-
 function lazyLoadImages() {
-	// console.log("loadem");
-	$("img.lazy").lazyload({
-		threshold: 200,
-		effect: "fadeIn",
-		failure_limit: 100000
-	}).removeClass("lazy");
+  // console.log("loadem");
+  $("img.lazy")
+    .lazyload({
+      threshold: 200,
+      effect: "fadeIn",
+      failure_limit: 100000
+    })
+    .removeClass("lazy");
 
-	setTimeout(lazyLoadImages, 1000);
+  setTimeout(lazyLoadImages, 1000);
 }
 setTimeout(lazyLoadImages, 1000);
 
-
-
 Template.post_list.rendered = function() {
+  let posts = this.find(".posts");
 
-	let posts = this.find(".posts");
+  window.isotope = new Isotope(posts, {
+    itemSelector: ".post",
+    sortBy: "original-order",
+    transitionDuration: 0,
+    masonry: {
+      isFitWidth: true
+    }
+  });
 
-	window.isotope = new Isotope(posts, {
-		itemSelector: ".post",
-		sortBy: "original-order",
-		transitionDuration: 0,
-		masonry: {
-			isFitWidth: true
-		},
-	});
+  function relayoutIsotope() {
+    // console.log("isotope reload");
+    window.isotope.reloadItems();
+    window.isotope.arrange({ sortBy: "original-order" });
+  }
+  setInterval(relayoutIsotope, 500);
 
-	function relayoutIsotope() {
-		// console.log("isotope reload");
-		window.isotope.reloadItems();
-		window.isotope.arrange({ sortBy: "original-order" });
-	}
-	setInterval(relayoutIsotope, 500);
+  let observer = new MutationObserver(function(/*mutations*/) {
+    _.debounce(relayoutIsotope, 500, true);
 
-	let observer = new MutationObserver(function( /*mutations*/ ) {
-		_.debounce(relayoutIsotope, 500, true);
+    $(posts)
+      .find("video")
+      .on("loadeddata", function() {
+        _.debounce(relayoutIsotope, 500, true);
+      });
 
-		$(posts).find("video").on("loadeddata", function() {
-			_.debounce(relayoutIsotope, 500, true);
-		});
+    $(posts)
+      .find("img")
+      .on("load", function() {
+        _.debounce(relayoutIsotope, 500, true);
+      });
+  });
 
-		$(posts).find("img").on("load", function() {
-			_.debounce(relayoutIsotope, 500, true);
-		});
-	});
-
-	observer.observe(posts, {
-		childList: true,
-		// subtree: true,,
-	});
+  observer.observe(posts, {
+    childList: true
+    // subtree: true,,
+  });
 };
 
 Template.post.helpers({
-	mediaCount() {
-		// console.log(this.cloudinary_media);
-		let items = _.pluck(this.cloudinary_media, "resource_type");
-		items = _.filter(items, function(item) {
-			return item;
-		});
-		return items.length;
-	},
+  mediaCount() {
+    // console.log(this.cloudinary_media);
+    let items = _.pluck(this.cloudinary_media, "resource_type");
+    items = _.filter(items, function(item) {
+      return item;
+    });
+    return items.length;
+  },
 
-	userCanEdit() {
-		return postEditableBy(this, Meteor.userId());
-		// return true;
-	},
+  userCanEdit() {
+    return postEditableBy(this, Meteor.userId());
+    // return true;
+  },
 
-	votedPretty() {
-		let post = Posts.findOne(this._id);
-		if (!post || _.where(post.votes, { voter_id: Meteor.userId(), category: "pretty" }).length === 0) {
-			return "";
-		} else {
-			return "voted";
-		}
-	},
+  userCanComment() {
+    // return !!Meteor.userId();
+    return true;
+  },
 
-	votedNerdy() {
-		let post = Posts.findOne(this._id);
-		if (!post || _.where(post.votes, { voter_id: Meteor.userId(), category: "nerdy" }).length === 0) {
-			return "";
-		} else {
-			return "voted";
-		}
-	},
+  votedPretty() {
+    let post = Posts.findOne(this._id);
+    if (
+      !post ||
+      _.where(post.votes, { voter_id: Meteor.userId(), category: "pretty" })
+        .length === 0
+    ) {
+      return "";
+    } else {
+      return "voted";
+    }
+  },
 
-	votedFunny() {
-		let post = Posts.findOne(this._id);
-		if (!post || _.where(post.votes, { voter_id: Meteor.userId(), category: "funny" }).length === 0) {
-			return "";
-		} else {
-			return "voted";
-		}
-	},
+  votedNerdy() {
+    let post = Posts.findOne(this._id);
+    if (
+      !post ||
+      _.where(post.votes, { voter_id: Meteor.userId(), category: "nerdy" })
+        .length === 0
+    ) {
+      return "";
+    } else {
+      return "voted";
+    }
+  },
 
-	server() {
-		if (window) {
-			return `http://${window.location.host}`;
-		}
-		return "http://localhost:3000";
-	}
+  votedFunny() {
+    let post = Posts.findOne(this._id);
+    if (
+      !post ||
+      _.where(post.votes, { voter_id: Meteor.userId(), category: "funny" })
+        .length === 0
+    ) {
+      return "";
+    } else {
+      return "voted";
+    }
+  },
 
+  server() {
+    if (window) {
+      return `http://${window.location.host}`;
+    }
+    return "http://localhost:3000";
+  }
 });
 
 Template.post.events({
-	"click .poster-link": function(event) {
-		$("body").addClass("no-scroll");
-		Session.set("previewing_post", this._id);
-		event.preventDefault();
-	},
+  "click .poster-link": function(event) {
+    $("body").addClass("no-scroll");
+    Session.set("previewing_post", this._id);
+    event.preventDefault();
+  },
 
-	"click .remove-post": function() {
-		let c = confirm("Delete post?");
-		if (!c) {
-			return;
-		}
-		Meteor.call("posts.remove", this._id);
-	},
+  "click .remove-post": function() {
+    let c = confirm("Delete post?");
+    if (!c) {
+      return;
+    }
+    Meteor.call("posts.remove", this._id);
+  },
 
-	"click .edit-post": function() {
-		Session.set("editing_post", this._id);
-	},
+  "click .edit-post": function() {
+    Session.set("editing_post", this._id);
+  },
 
-	"click .vote-pretty": function() {
-		// console.log("vote pretty");
-		Meteor.call("posts.vote", this._id, "pretty");
-	},
+  "click .vote-pretty": function() {
+    // console.log("vote pretty");
+    Meteor.call("posts.vote", this._id, "pretty");
+  },
 
-	"click .vote-nerdy": function() {
-		// console.log("vote nerdy");
-		Meteor.call("posts.vote", this._id, "nerdy");
-	},
+  "click .vote-nerdy": function() {
+    // console.log("vote nerdy");
+    Meteor.call("posts.vote", this._id, "nerdy");
+  },
 
-	"click .vote-funny": function() {
-		// console.log("vote funny");
-		Meteor.call("posts.vote", this._id, "funny");
-	}
+  "click .vote-funny": function() {
+    // console.log("vote funny");
+    Meteor.call("posts.vote", this._id, "funny");
+  }
 });
 
-
 Template.post_overlay.helpers({
-	post() {
-		let _id = this.post_id;
-		return Posts.findOne(_id);
-	},
+  post() {
+    let _id = this.post_id;
+    return Posts.findOne(_id);
+  },
 
-	server() {
-		if (window) {
-			return `http://${window.location.host}`;
-		}
-		return "http://localhost:3000";
-	}
+  server() {
+    if (window) {
+      return `http://${window.location.host}`;
+    }
+    return "http://localhost:3000";
+  }
 });
 
 Template.post_overlay.events({
-	"click .overlay, click .close-overlay": function(event) {
-		if (event.target !== event.currentTarget) {
-			return;
-		}
-		$("body").removeClass("no-scroll");
-		Session.set("previewing_post", false);
-	},
-
-
+  "click .overlay, click .close-overlay": function(event) {
+    if (event.target !== event.currentTarget) {
+      return;
+    }
+    $("body").removeClass("no-scroll");
+    Session.set("previewing_post", false);
+  }
 });
 
 AutoForm.hooks({
-	"updatePostForm": {
-		"onSuccess": function(formType, result) {
-			Meteor.call("posts.mark_posted", this.docId);
-			Session.set("editing_post", false);
-		},
-		"onError": function(formType, result) {},
-	},
+  updatePostForm: {
+    onSuccess: function(formType, result) {
+      Meteor.call("posts.mark_posted", this.docId);
+      Session.set("editing_post", false);
+    },
+    onError: function(formType, result) {}
+  },
 
-	"updatePrefsForm": {
-		"before": {
-			update: function(doc) {
-				// console.log("before", doc);
-				// console.log(doc.$set.weeks);
-				doc.$set.weeks = _.sortBy(doc.$set.weeks, "start");
+  updatePrefsForm: {
+    before: {
+      update: function(doc) {
+        // console.log("before", doc);
+        // console.log(doc.$set.weeks);
+        doc.$set.weeks = _.sortBy(doc.$set.weeks, "start");
 
-				this.result(doc);
-			}
-		},
-		"onError": function(insertDoc, updateDoc, currentDoc) {
-			// console.log(JSON.stringify(this.updateDoc));
-			this.event.preventDefault();
-			return false;
-		},
-	},
+        this.result(doc);
+      }
+    },
+    onError: function(insertDoc, updateDoc, currentDoc) {
+      // console.log(JSON.stringify(this.updateDoc));
+      this.event.preventDefault();
+      return false;
+    }
+  }
 });
 
 function uploadFile(post, slot, files) {
+  let maxImageFileSizeMB = 10;
+  let maxVideoFileSizeMB = 20;
 
-	let maxImageFileSizeMB = 10;
-	let maxVideoFileSizeMB = 20;
+  let isImage = false;
+  let isVideo = false;
 
-	let isImage = false;
-	let isVideo = false;
+  if (_.contains(["image/png", "!image/gif", "image/jpeg"], files[0].type)) {
+    isImage = true;
+  }
 
-	if (_.contains(["image/png", "!image/gif", "image/jpeg"], files[0].type)) {
-		isImage = true;
-	}
+  if (_.contains(["video/mp4", "video/quicktime"], files[0].type)) {
+    isVideo = true;
+  }
 
-	if (_.contains(["video/mp4", "video/quicktime"], files[0].type)) {
-		isVideo = true;
-	}
+  console.log(files[0].type, files[0].size / 1024 / 1024, isImage, isVideo);
 
-	console.log(files[0].type, files[0].size / 1024 / 1024, isImage, isVideo);
+  if (!isImage && !isVideo) {
+    alert(
+      `Your file is not of a recognized type.\nImages must be formatted as .png or .jpg.\nVideos must be formatted as .mp4., .m4v, .mov.\nDon't use .gif`
+    );
+    return;
+  }
 
-	if (!isImage && !isVideo) {
-		alert(`Your file is not of a recognized type.\nImages must be formatted as .png or .jpg.\nVideos must be formatted as .mp4., .m4v, .mov.\nDon't use .gif`);
-		return;
-	}
+  if (isImage && files[0].size > maxImageFileSizeMB * 1024 * 1024) {
+    alert(
+      `Your file is too large.\nImages must be under ${maxImageFileSizeMB} mb.`
+    );
+    return;
+  }
 
-	if (isImage && (files[0].size > maxImageFileSizeMB * 1024 * 1024)) {
-		alert(`Your file is too large.\nImages must be under ${maxImageFileSizeMB} mb.`);
-		return;
-	}
+  if (isVideo && files[0].size > maxVideoFileSizeMB * 1024 * 1024) {
+    alert(
+      `Your file is too large.\nVideos must be under ${maxVideoFileSizeMB} mb.`
+    );
+    return;
+  }
 
-	if (isVideo && (files[0].size > maxVideoFileSizeMB * 1024 * 1024)) {
-		alert(`Your file is too large.\nVideos must be under ${maxVideoFileSizeMB} mb.`);
-		return;
-	}
-
-
-	Cloudinary.upload(files, {
-		folder: Meteor.settings.public.cloudinary_folder,
-		resource_type: "auto"
-	}, (err, res) => {
-		// console.log("Upload Error:", err);
-		// console.log("Upload Result:", res);
-		if (!err) {
-			Meteor.call("posts.updateMedia", post, slot, res);
-		}
-	});
+  Cloudinary.upload(
+    files,
+    {
+      folder: Meteor.settings.public.cloudinary_folder,
+      resource_type: "auto"
+    },
+    (err, res) => {
+      // console.log("Upload Error:", err);
+      // console.log("Upload Result:", res);
+      if (!err) {
+        Meteor.call("posts.updateMedia", post, slot, res);
+      }
+    }
+  );
 }
 
-
 Template.edit_post_form.events({
-	"click .cancel": function() {
-		if (!this.posted) {
-			Meteor.call("posts.remove", this._id);
-		}
-		Session.set("editing_post", false);
-	},
+  "click .cancel": function() {
+    if (!this.posted) {
+      Meteor.call("posts.remove", this._id);
+    }
+    Session.set("editing_post", false);
+  },
 
-	"click .submit": function() {
-		// handled through autoform hooks
-	},
+  "click .submit": function() {
+    // handled through autoform hooks
+  },
 
-	"click .remove-file-0": function() {
-		Meteor.call("posts.updateMedia", this.post_id, 0, {});
-	},
+  "click .remove-file-0": function() {
+    Meteor.call("posts.updateMedia", this.post_id, 0, {});
+  },
 
-	"click .remove-file-1": function() {
-		Meteor.call("posts.updateMedia", this.post_id, 1, {});
-	},
+  "click .remove-file-1": function() {
+    Meteor.call("posts.updateMedia", this.post_id, 1, {});
+  },
 
-	"click .remove-file-2": function() {
-		Meteor.call("posts.updateMedia", this.post_id, 2, {});
-	},
+  "click .remove-file-2": function() {
+    Meteor.call("posts.updateMedia", this.post_id, 2, {});
+  },
 
-	"change .upload-file-0": function(event, template) {
-		let post = this.post_id;
-		let slot = 0;
-		let files = template.find(".upload-file-0").files;
-		uploadFile(post, slot, files);
-	},
+  "change .upload-file-0": function(event, template) {
+    let post = this.post_id;
+    let slot = 0;
+    let files = template.find(".upload-file-0").files;
+    uploadFile(post, slot, files);
+  },
 
-	"change .upload-file-1": function(event, template) {
-		let post = this.post_id;
-		let slot = 1;
-		let files = template.find(".upload-file-1").files;
-		uploadFile(post, slot, files);
-	},
+  "change .upload-file-1": function(event, template) {
+    let post = this.post_id;
+    let slot = 1;
+    let files = template.find(".upload-file-1").files;
+    uploadFile(post, slot, files);
+  },
 
-	"change .upload-file-2": function(event, template) {
-		let post = this.post_id;
-		let slot = 2;
-		let files = template.find(".upload-file-2").files;
-		uploadFile(post, slot, files);
-	},
+  "change .upload-file-2": function(event, template) {
+    let post = this.post_id;
+    let slot = 2;
+    let files = template.find(".upload-file-2").files;
+    uploadFile(post, slot, files);
+  }
 
-	// "change .create-post-file": function(event, template) {
-	//
-	// 	event.preventDefault();
-	//
-	// 	Meteor.call("posts.insert", {}, function(error, id){
-	// 		if (error) {
-	// 			console.error(error);
-	// 			return;
-	// 		}
-	// 		// upload image + add to post
-	// 		let files = template.find(".create-post-file").files;
-	// 		Cloudinary.upload(files, {
-	// 			folder: "avalanche",
-	// 			resource_type: "auto"
-	// 		}, (err, res) => {
-	// 			console.log("Upload Error:", err);
-	// 			console.log("Upload Result:", res);
-	// 			if (!err) {
-	// 				Meteor.call("posts.updateMedia", id, res);
-	// 			}
-	// 		});
-	// 	});
-	//
-	//
-	// },,,
+  // "change .create-post-file": function(event, template) {
+  //
+  // 	event.preventDefault();
+  //
+  // 	Meteor.call("posts.insert", {}, function(error, id){
+  // 		if (error) {
+  // 			console.error(error);
+  // 			return;
+  // 		}
+  // 		// upload image + add to post
+  // 		let files = template.find(".create-post-file").files;
+  // 		Cloudinary.upload(files, {
+  // 			folder: "avalanche",
+  // 			resource_type: "auto"
+  // 		}, (err, res) => {
+  // 			console.log("Upload Error:", err);
+  // 			console.log("Upload Result:", res);
+  // 			if (!err) {
+  // 				Meteor.call("posts.updateMedia", id, res);
+  // 			}
+  // 		});
+  // 	});
+  //
+  //
+  // },,,
 });
 
 Template.edit_post_form.helpers({
-	post() {
-		// console.log("hi", this.post_id);
-		return Posts.findOne(this.post_id);
-	},
-	lessons() {
+  post() {
+    // console.log("hi", this.post_id);
+    return Posts.findOne(this.post_id);
+  },
+  lessons() {
+    let p = getPrefs();
+    if (!p) {
+      return {};
+    }
 
-		let p = getPrefs();
-		if (!p) {
-			return {};
-		}
-
-		let lessons = _.pluck(p.weeks, "topic");
-		lessons = _.object(lessons, lessons);
-		return lessons;
-	},
+    let lessons = _.pluck(p.weeks, "topic");
+    lessons = _.object(lessons, lessons);
+    return lessons;
+  }
 });
--- avalanche/imports/ui/users.js
+++ avalanche/imports/ui/users.js
@@ -3,204 +3,201 @@ import "./users.html";
 import { Posts } from "../api/posts.js";
 import { currentWeek } from "../api/prefs.js";
 
-
 Meteor.subscribe("users");
 
 Template.user_summaries.helpers({
-	users() {
-		let users = Meteor.users.find({});
+  users() {
+    let users = Meteor.users.find({});
 
-		users = users.fetch();
-		_.each(users, function(user) {
-			user.sort_by = posts_this_week(user._id);
+    users = users.fetch();
+    _.each(users, function(user) {
+      user.sort_by = posts_this_week(user._id);
 
+      if (user.profile.first_name == "Justin") {
+        user.sort_by = -10;
+      }
+      if (user.profile.first_name == "Brinna") {
+        user.sort_by = -5;
+      }
+    });
 
-			if (user.profile.first_name == "Justin") {
-				user.sort_by = -10;
-			}
-			if (user.profile.first_name == "Brinna") {
-				user.sort_by = -5;
-			}
-		});
+    users = _.reject(users, function(user) {
+      let drops = [
+        "Aaron",
+        "Lauren",
+        "Terricka",
+        "Aimee",
+        "Chao",
+        "Neena",
+        "Nour"
+      ];
+      return _.contains(drops, user.profile.first_name);
+    });
 
+    users = _.sortBy(users, "sort_by");
+    users.reverse();
 
-
-		users = _.reject(users, function(user) {
-			let drops = ["Aaron", "Lauren", "Terricka", "Aimee", "Chao"];
-			return _.contains(drops, user.profile.first_name);
-		});
-
-		users = _.sortBy(users, "sort_by");
-		users.reverse();
-
-
-
-		// console.log(users);
-		// console.log(users);
-		return users;
-	}
+    // console.log(users);
+    // console.log(users);
+    return users;
+  }
 });
 
-
 function posts_this_week(user) {
-	let week = currentWeek();
+  let week = currentWeek();
 
-	if (!week) {
-		return;
-	}
+  if (!week) {
+    return;
+  }
 
-	let posts = Posts.find({
-		"author_id": user,
-		"created_at": {
-			$gte: week.start,
-			$lt: week.end,
-		}
-	});
+  let posts = Posts.find({
+    author_id: user,
+    created_at: {
+      $gte: week.start,
+      $lt: week.end
+    }
+  });
 
-
-
-	return posts.count();
+  return posts.count();
 }
 
-
 Template.user_summary.helpers({
-	posts_total() {
+  posts_total() {
+    let posts = Posts.find({
+      author_id: this._id
+    });
+    return posts.count();
+  },
 
-		let posts = Posts.find({
-			"author_id": this._id,
-		});
-		return posts.count();
-	},
+  posts_this_week() {
+    let posts = posts_this_week(this._id);
+    return posts;
 
-	posts_this_week() {
-		let posts = posts_this_week(this._id);
-		return posts;
-
-		//
-		//
-		// let week = currentWeek();
-		//
-		// if (!week) {
-		// 	return;
-		// }
-		//
-		//
-		//
-		// let posts = Posts.find({
-		// 	"author_id": this._id,
-		// 	"created_at":
-		// 	{
-		// 		$gte: week.start,
-		// 		$lt: week.end,
-		// 	}
-		// });
-		//
-		//
-		//
-		// return posts.count();
-	}
+    //
+    //
+    // let week = currentWeek();
+    //
+    // if (!week) {
+    // 	return;
+    // }
+    //
+    //
+    //
+    // let posts = Posts.find({
+    // 	"author_id": this._id,
+    // 	"created_at":
+    // 	{
+    // 		$gte: week.start,
+    // 		$lt: week.end,
+    // 	}
+    // });
+    //
+    //
+    //
+    // return posts.count();
+  }
 });
 
 Template.edit_user_form.helpers({
-	user() {
-		return Meteor.users.findOne(this.user_id);
-	}
+  user() {
+    return Meteor.users.findOne(this.user_id);
+  }
 });
 
-
-
 Template.edit_user_form.events({
-	"click .cancel": function() {
-		event.preventDefault();
-		Session.set("editing_user", false);
-	},
+  "click .cancel": function() {
+    event.preventDefault();
+    Session.set("editing_user", false);
+  },
 
-	"click .submit": function(event, template) {
-		event.preventDefault();
-		// console.log("submit", this._id);
-		let first_name = template.find("#first-name").value || "Unknown";
-		let last_name = template.find("#last-name").value || "Author";
-		let description = template.find("#description").value || "";
+  "click .submit": function(event, template) {
+    event.preventDefault();
+    // console.log("submit", this._id);
+    let first_name = template.find("#first-name").value || "Unknown";
+    let last_name = template.find("#last-name").value || "Author";
+    let description = template.find("#description").value || "";
 
-		Meteor.call("users.updateName", {
-			user_id: this._id,
-			first_name: first_name,
-			last_name: last_name
-		});
+    Meteor.call("users.updateName", {
+      user_id: this._id,
+      first_name: first_name,
+      last_name: last_name
+    });
 
-		Meteor.call("users.updateDescription", {
-			user_id: this._id,
-			description: description
-		});
+    Meteor.call("users.updateDescription", {
+      user_id: this._id,
+      description: description
+    });
 
-		//make sure it worked!
-		Session.set("editing_user", false);
-	},
+    //make sure it worked!
+    Session.set("editing_user", false);
+  },
 
-	"change .upload-headshot-file": function(event, template) {
+  "change .upload-headshot-file": function(event, template) {
+    event.preventDefault();
+    // console.log("upload", this);
+    let files = template.find(".upload-headshot-file").files;
+    Cloudinary.upload(
+      files,
+      {
+        folder: Meteor.settings.public.cloudinary_folder,
+        resource_type: "image"
+      },
+      (err, res) => {
+        console.log("Upload Error:", err);
+        console.log("Upload Result:", res);
+        console.log(res);
 
-		event.preventDefault();
-		// console.log("upload", this);
-		let files = template.find(".upload-headshot-file").files;
-		Cloudinary.upload(files, {
-			folder: Meteor.settings.public.cloudinary_folder,
-			resource_type: "image",
-		}, (err, res) => {
-			console.log("Upload Error:", err);
-			console.log("Upload Result:", res);
-			console.log(res);
+        let data = {
+          user_id: this._id,
+          res
+        };
+        if (!err) {
+          console.log("data in", data);
+          Meteor.call("users.updateHeadshot", data);
+        }
+      }
+    );
+  },
 
-			let data = {
-				user_id: this._id,
-				res,
-			};
-			if (!err) {
-				console.log("data in", data);
-				Meteor.call("users.updateHeadshot", data);
-			}
-		});
+  "change .upload-file": function(event, template) {
+    event.preventDefault();
+    console.log("upload", this);
 
-	},
+    let files = template.find(".upload-file").files;
+    Cloudinary.upload(
+      files,
+      {
+        folder: Meteor.settings.public.cloudinary_folder,
+        resource_type: "image"
+      },
+      (err, res) => {
+        console.log("Upload Error:", err);
+        console.log("Upload Result:", res);
+        console.log(res);
 
+        let data = {
+          user_id: this.user_id,
+          res
+        };
+        if (!err) {
+          console.log("data in", data);
+          Meteor.call("users.updateHeadshot", data);
+        }
+      }
+    );
 
-	"change .upload-file": function(event, template) {
-		event.preventDefault();
-		console.log("upload", this);
+    // console.log("clicknow");
+    //
+    // let files = template.find(".upload-file").files;
+    // Cloudinary.upload(files, {
+    // 	folder: "avalanche",
+    // 	resource_type: "auto"
+    // }, (err, res) => {
+    // 	console.log("Upload Error:", err);
+    // 	console.log("Upload Result:", res);
+    // 	if (!err) {
 
-		let files = template.find(".upload-file").files;
-		Cloudinary.upload(files, {
-			folder: Meteor.settings.public.cloudinary_folder,
-			resource_type: "image",
-		}, (err, res) => {
-			console.log("Upload Error:", err);
-			console.log("Upload Result:", res);
-			console.log(res);
-
-			let data = {
-				user_id: this.user_id,
-				res,
-			};
-			if (!err) {
-				console.log("data in", data);
-				Meteor.call("users.updateHeadshot", data);
-			}
-		});
-
-
-		// console.log("clicknow");
-		//
-		// let files = template.find(".upload-file").files;
-		// Cloudinary.upload(files, {
-		// 	folder: "avalanche",
-		// 	resource_type: "auto"
-		// }, (err, res) => {
-		// 	console.log("Upload Error:", err);
-		// 	console.log("Upload Result:", res);
-		// 	if (!err) {
-
-		// 	}
-		// });
-
-	},
-
+    // 	}
+    // });
+  }
 });
